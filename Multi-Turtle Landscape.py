ccf5f27c5419e213d191a5113e21712ecddbf723
kumaren14 <kumaren.a@gmail.com>
2019-09-23 15:32:48 -0500

    I created this project to draw a landscape with four turtles
    Not sure yet what kind of landscape or how i'm going to draw it, but we'll see

---
+++ Multi-Turtle Landscape.py
@@ -0,0 +1,28 @@
+import turtle
+
+ian = turtle.Turtle()
+alan = turtle.Turtle()
+ellie = turtle.Turtle()
+timmy = turtle.Turtle()
+
+ian.shape("turtle")
+alan.shape("turtle")
+ellie.shape("turtle")
+timmy.shape("turtle")
+
+ian.penup()
+alan.penup()
+ellie.penup()
+timmy.penup()
+
+ian.goto(-500, -300)
+
+ian.color("gray")
+ian.pendown()
+
+ian.left(55)
+ian.forward(525)
+ian.right
+
+
+turtle.exitonclick()
