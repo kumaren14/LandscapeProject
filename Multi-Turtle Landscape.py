de4579a3d0bfb667d9e65d9e5692ee4e3d66f141
kumaren14 <kumaren.a@gmail.com>
2019-09-23 15:37:08 -0500

    I created this project to draw a landscape with four turtles
    I have started work on the mountains, and have a shape in mind

--- Multi-Turtle Landscape.py
+++ Multi-Turtle Landscape.py
@@ -5,6 +5,11 @@ alan = turtle.Turtle()
 ellie = turtle.Turtle()
 timmy = turtle.Turtle()
 
+ian.speed(10)
+alan.speed(10)
+ellie.speed(10)
+timmy.speed(10)
+
 ian.shape("turtle")
 alan.shape("turtle")
 ellie.shape("turtle")
@@ -22,7 +27,12 @@ ian.pendown()
 
 ian.left(55)
 ian.forward(525)
+ian.right(120)
+ian.forward(475)
+ian.left(145)
 ian.right
+
+ian.forward(325)
 
 
 turtle.exitonclick()
